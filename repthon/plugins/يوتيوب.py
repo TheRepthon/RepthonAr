839c3f46cc9d7d9269687a1d78af507e64096084
Repthon-Ar <alfashymwswlyny@gmail.com>
2026-03-01 10:41:18 +0300

    Update يوتيوب.py

--- repthon/plugins/يوتيوب.py
+++ repthon/plugins/يوتيوب.py
@@ -499,7 +499,7 @@ async def _(event): #Code by T.me/RR0RT
     else:
         return await edit_or_reply(event, "**⎉╎قم باضافـة إسـم للامـر ..**\n**⎉╎بحث + اسـم المقطـع الصـوتي**")
     revent = await edit_or_reply(event, "**╮ جـارِ البحث ؏ـن المقطـٓع الصٓوتـي... 🎧♥️╰**")
-    ydl_opts = {
+    ydl_ops = {
     "format": "bestaudio/best",
     "keepvideo": False,
     "prefer_ffmpeg": True,
